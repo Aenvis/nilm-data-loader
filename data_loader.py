699c7b08c1e803c48c0ec34012e351779388525d
Wojtek Czerwinski <czerwinskiwtj@gmail.com>
2023-04-26 21:03:10 +0200

    Plot now displays hours as x axis; Add render of avg power consumption to the plot

--- data_loader.py
+++ data_loader.py
@@ -1,14 +1,33 @@
-from datetime import datetime
 import pandas as pd 
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def load(path: str) -> pd.DataFrame:
-    data = pd.read_csv(path, header=None, delimiter=';', names=['date', 'measurment1', 'measurment8'])
-    #data['date'] = np.asarray(data['date'], dtype='datetime64[s]')
+    data = pd.read_csv(path, header=None, delimiter=';', names=['date', 'measurement1', 'measurement8'])
+    data['date'] = pd.to_datetime(data['date'], format='%d/%m/%Y %H:%M:%S')
+    data['time'] = data['date'].dt.time
     return data
 
 def plot(data: pd.DataFrame) -> None:
-    #plt.xticks(data['date'], rotation=90)
-    plt.plot(data['measurment8'])
+    # Set the index to the 'date' column
+    data = data.set_index('date')
+    # Plot the 'measurement8' column
+    plt.plot(data['measurement1'])
+    # Set the x-axis tick marks to every 2 hours
+    x_ticks = pd.date_range(start=data.index.max() - pd.Timedelta(hours=23), end=data.index.max(), freq='2H')
+    plt.xticks(x_ticks, x_ticks.strftime('%H:%M:%S'), rotation=45, ha='right', fontsize=8)
+    # Set the x-axis label
+    plt.xlabel('Time')
+    # Set the y-axis label
+    plt.ylabel('Measurement 1')
+
+    last_measurement = data['measurement1'].iloc[-1]
+    top_position = np.max(data['measurement1'])
+    plt.text(data.index[len(data['time'])/2], top_position, get_avg_measurment(data))
+
+    # Show the plot
     plt.show()
+
+def get_avg_measurment(data: pd.DataFrame) -> str:
+    return f'avg: {"{:.2f}".format(np.mean(data["measurement1"]))}'
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import data_loader as dl
 
 def main():
     # here R before the string marks it as 'raw', so it interprets \t as a text
-    data = dl.load(R'data\tracebase-master\complete\Amplifier\dev_AF6490_2011.12.28.csv')
+    data = dl.load(R'data\tracebase-master\complete\Freezer\dev_D36601_2011.12.16.csv')
     dl.plot(data)
 
 
