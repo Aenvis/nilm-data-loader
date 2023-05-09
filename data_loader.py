3be510a0b38e8a5ec4710bb5ecf0776aa36171a0
Wojtek Czerwinski <czerwinskiwtj@gmail.com>
2023-05-09 18:57:13 +0200

    Code cleanup, mode standardized syntax

--- data_loader.py
+++ data_loader.py
@@ -12,31 +12,30 @@ def load(path: str) -> pd.DataFrame:
 def plot(data: pd.DataFrame) -> None:
     # Set the index to the 'date' column
     data = data.set_index('date')
-    # Plot the 'measurement8' column
+
+    # Plot the 'measurement1' column
     plt.plot(data['measurement1'])
+    
     # Set the x-axis tick marks to every 2 hours
     x_ticks = pd.date_range(start=data.index.max() - pd.Timedelta(hours=23), end=data.index.max(), freq='2H')
     plt.xticks(x_ticks, x_ticks.strftime('%H:%M:%S'), rotation=45, ha='right', fontsize=8)
-    # Set the x-axis label
+
     plt.xlabel('Time')
-    # Set the y-axis label
     plt.ylabel('Measurement 1')
 
     top_position = np.max(data['measurement1'])
-    plt.text(data.index[len(data['time'])//2], top_position, get_avg_measurment(data))
-
-    plt.text(data.index[len(data['time'])//2], top_position - 200, get_max_measurment(data))
-
-
-    plt.text(data.index[len(data['time']) // 2], top_position - 100, get_number_of_starts(data))
+    plt.text(data.index[len(data['time']) // 2],   top_position,      f'avg: {"{:.2f}".format(get_avg_measurment(data))}')
+    plt.text(data.index[len(data['time']) // 2],   top_position - 50, f'max: {"{:.2f}".format(get_max_measurment(data))}')
+    plt.text(data.index[len(data['time']) // 2],   top_position - 60, f'num of triggers: {get_number_of_starts(data)}')
     # Show the plot
     plt.show()
 
-def get_avg_measurment(data: pd.DataFrame) -> str:
-    return f'avg: {"{:.2f}".format(np.mean(data["measurement1"]))}'
+def get_avg_measurment(data: pd.DataFrame) -> float:
+    return np.mean(data["measurement1"])
 
-def get_max_measurment(data: pd.DataFrame) -> str:
-    return f'max: {"{:.2f}".format(np.max(data["measurement1"]))}'
+def get_max_measurment(data: pd.DataFrame) -> float:
+    return np.max(data["measurement1"])
+
 def get_number_of_starts(data: pd.DataFrame) -> int:
     avg = np.mean(data["measurement1"])
     print(avg)
