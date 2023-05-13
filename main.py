14893da82ed3d997a0510d8c1d6fbb0d66e741bd
Dominik Wójtowicz <soltyspozdro@gmail.com>
2023-05-13 23:10:46 +0200

    added subplot with aggregated and device power

--- main.py
+++ main.py
@@ -3,34 +3,12 @@ from pandas import read_parquet, to_datetime, DataFrame
 import data_loader as dl
 import matplotlib.pyplot as plt
 
-def get_avg_measurment(data: DataFrame) -> str:
-    return f'avg: {"{:.2f}".format(mean(data["active_power"]))}'
-
-def get_max_measurment(data: DataFrame) -> str:
-    return f'max: {"{:.2f}".format(max(data["active_power"]))}'
 def main():
 
 
-    # data = dl.load_enertalk(R'data\enertalk\20161006\01_fridge.parquet.gzip')
-    data = dl.load_enertalk(R'data\enertalk\20161006\00_total.parquet.gzip')
-    # Extract the 'active_power' and 'hour' column data
-    active_power = data['active_power']
-    hour = data['time']
-
-    # Create a plot using Matplotlib
-    plt.plot(hour, active_power)
-    plt.xlabel('Hour')
-    plt.ylabel('Active Power')
-    plt.title('Fridge usage')
-
-    top_position = max(data['active_power'])
-    print(get_avg_measurment(data))
-    print(get_max_measurment(data))
-    plt.show()
-
-
-
-     # dl.plot(data)
+    data = dl.load_enertalk(R'data\enertalk\20161006\01_fridge.parquet.gzip')
+    # data = dl.load_enertalk(R'data\enertalk\20161006\00_total.parquet.gzip')
+    dl.plot_enertalk(data, "Fridge", '20161006')
 
 
 if __name__ == '__main__':
